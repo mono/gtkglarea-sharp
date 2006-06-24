5b8de6ab7be5f1481342db056bc68334ef3ea5a3
C.J. Adams-Collier <cjac@colliertech.org>
2006-06-24 15:09:52 +0000

    - removed explicit LGPL bits
    svn path=/trunk/gtkglarea-sharp/; revision=62015

--- examples/trackball/Trackball.cs
+++ examples/trackball/Trackball.cs
@@ -11,31 +11,6 @@
  * written prior permission.
  */
  
-/*
-        Those parts not created by Silicon Graphics, Inc. are
-        Copyright (C) 2005 C.J. Collier <cjcollier@colliertech.org>
-
-        This program is free software; you can redistribute it and/or
-        modify it under the terms of the GNU Lesser General Public License
-        as published by the Free Software Foundation.
-
-        This program is distributed in the hope that it will be useful,
-        but WITHOUT ANY WARRANTY; without even the implied warranty of
-        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-        See the GNU Lesser General Public License for more details.
-        http://www.gnu.org/licenses/lgpl.html
-
-        You should have received a copy of the GNU Lesser General Public License
-        along with this program; if not, write to:
-
-                Free Software Foundation, Inc.
-                59 Temple Place - Suite 330
-                Boston, MA  02111-1307, USA
-
-*/
-
- 
 using System;
 
 public class Trackball {
