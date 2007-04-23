423af5ca4b1e04fc60210133c2b0b99dc844e29c
C.J. Adams-Collier <cjac@colliertech.org>
2007-04-23 22:53:37 +0000

    2007-04-23T06:35  C.J. Adams-Collier  <cjac@colliertech.org>         * gtkglarea/Makefile.am         - removed generated-stamp and generated/* from CLEANFILES         - added generated/* and generated-stamp to EXTRA_DIST so that the           build no longer requires that the builder have gapi         * configure.ac         - did a bunch of clean-up         * gtkglarea/generated         - Added gtkglarea/generated/*.cs to svn so they don't need to be           generated         * TODO         - added some things I'd like to do/see done
    svn path=/trunk/gtkglarea-sharp/; revision=76155

---
+++ gtkglarea/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly:AssemblyVersion("0.0.3.0")]
+[assembly:AssemblyDelaySign(false)]
+[assembly:AssemblyKeyFile("gtkglarea-sharp.snk")]
